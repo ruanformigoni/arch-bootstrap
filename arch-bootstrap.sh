7efcc5ba9a5f8f9670618a80444de8b7a137a2ae
Ruan E. Formigoni <ruanformigoni@gmail.com>
2025-12-25 17:19:14 -0300

    Included libseccomp

--- arch-bootstrap.sh
+++ arch-bootstrap.sh
@@ -25,6 +25,7 @@ PACMAN_PACKAGES=(
   acl archlinux-keyring attr brotli bzip2 curl expat glibc gpgme libarchive
   libassuan libgpg-error libnghttp2 libnghttp3 libssh2 lzo openssl pacman pacman-mirrorlist xz zlib
   krb5 e2fsprogs keyutils libidn2 libunistring gcc-libs lz4 libpsl icu libunistring zstd libxml2
+  libseccomp
 )
 BASIC_PACKAGES=(${PACMAN_PACKAGES[*]} filesystem base)
 EXTRA_PACKAGES=(coreutils bash grep gawk file tar gzip systemd sed)
